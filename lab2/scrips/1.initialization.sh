4c6fed21febba815abecf61b30639c1e89d38565
Anastasia Pavlova <99503656+Catwithraincoat@users.noreply.github.com>
2024-09-24 00:58:46 +0300

    Update 1.initialization.sh

--- lab2/scrips/1.initialization.sh
+++ lab2/scrips/1.initialization.sh
@@ -1,11 +1,11 @@
-export POSTGRES_USER=kitokato
+export POSTGRES_USER=postgres0
 export POSTGRES_ENCODING='WIN1251'
 export POSTGRES_LOCALE='ru_RU.CP1251'
 export POSTGRES_PORT=9653
 export PGDATA="$HOME/qcd16"
 
 mkdir   $PGDATA                     
-          
+adduser $POSTGRES_USER  
 chown   $POSTGRES_USER $PGDATA
 
 su      $POSTGRES_USER
