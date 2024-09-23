b6d20d6efed6133d7af079955f1cdb77eb071f1c
Anastasia Pavlova <99503656+Catwithraincoat@users.noreply.github.com>
2024-09-23 23:28:18 +0300

    Create 1.initialization.sh

---
+++ lab2/scrips/1.initialization.sh
@@ -0,0 +1,12 @@
+export POSTGRES_USER=postgres0
+export POSTGRES_CONFIG_DIRECTORY="./config"
+export POSTGRES_SUPER_USER_PASSWORD_FILE="$POSTGRES_CONFIG_DIRECTORY/postgres_su_password.txt"
+export POSTGRES_PORT=9653
+export PGDATA="$HOME/qcd16"
+
+mkdir   $PGDATA                     2> /dev/null
+
+adduser $POSTGRES_USER              2>/dev/null
+chown   $POSTGRES_USER $PGDATA
+
+su      $POSTGRES_USER
